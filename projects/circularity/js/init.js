3b155e8df534dc861bc0455ae0596bd96fbbc28c
ahill2023 <ahill7426@gmail.com>
2022-06-26 13:57:33 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,24 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle () {
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle);
+        }
         
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        var drawCircle = 0; 
+        while (drawCircle < 100) {
+            //idk what to do here//
+            drawCircle++
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -40,6 +51,12 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
+            for (var i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];
+                
+                physikz.updatePosition(eachCircle)
+                game.checkCirclePosition(eachCircle)
+             }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
@@ -60,7 +77,16 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
+            if ( circle.x < 0 ) {
+                circle.x = 0;
+            }
+            if ( circle.y < 0 ) {
+                circle.y = 0;
+            }
+
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
 
